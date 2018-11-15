4940626a29a642d4a35a4a2dc9f7d2064f4d9f7c
Emmad5 <emmadeas9@gmail.com>
2018-11-14 16:49:25 -0800

    Add additional functionality to log in and sign up pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
     render :new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+    user = User.new(user_params)
+    if user.save
+      login!(user)
       redirect_to cats_url
     else
-      flash.now[:errors] = @user.errors.full_messages
+      flash.now[:errors] = user.errors.full_messages
       render :new
     end
   end
